fd570c96f404215a66ef05ab8513d1c6b9e99205
Danny França <me@dannyfranca.com>
2023-07-09 08:24:14 +0100

    refactor: remove components from root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -1,17 +1,11 @@
-import { Counter } from "./components/counter/counter";
-import { Logo } from "./components/logo/logo";
-
 export default () => {
   return (
     <>
       <head>
         <meta charSet="utf-8" />
-        <title>Qwik Blank App</title>
+        <title>Qwik Router App</title>
       </head>
-      <body>
-        <Logo />
-        <Counter />
-      </body>
+      <body></body>
     </>
   );
 };

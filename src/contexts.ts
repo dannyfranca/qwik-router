e1ad0c6a5aed9036a72957edaabd774bbc8010aa
Danny França <me@dannyfranca.com>
2023-07-09 07:50:13 +0100

    feat: add lib contexts

---
+++ src/contexts.ts
@@ -0,0 +1,15 @@
+import { createContextId } from "@builder.io/qwik";
+
+import type { RouteNavigate, RouteParams, RouteState } from "./types";
+
+export const RouteStoreContext = createContextId<RouteState>(
+  "com.dannyfranca.route.store"
+);
+
+export const RouteNavigateContext = createContextId<RouteNavigate>(
+  "com.dannyfranca.route.navigate"
+);
+
+export const RouteParamsContext = createContextId<RouteParams>(
+  "com.dannyfranca.route.params"
+);

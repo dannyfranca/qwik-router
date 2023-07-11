01f8c5a5250bb3ba1ea14cd25cb69d5a31570c00
Danny França <me@dannyfranca.com>
2023-07-11 21:04:24 +0100

    feat: add copy file to build

--- packages/qwik-router/vite.config.ts
+++ packages/qwik-router/vite.config.ts
@@ -1,56 +1,62 @@
-import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
-import dts from "vite-plugin-dts";
-import devkit from "@nx/devkit";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import { qwikNxVite } from "qwik-nx/plugins";
+import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import devkit from '@nx/devkit';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikNxVite } from 'qwik-nx/plugins';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(() => {
   return {
     build: {
       lib: {
-        name: "qwik-router",
-        entry: "./src/index.ts",
-        formats: ["es", "cjs"],
-        fileName: "index",
+        name: 'qwik-router',
+        entry: './src/index.ts',
+        formats: ['es', 'cjs'],
+        fileName: 'index',
       },
       rollupOptions: {
-        external: ["@builder.io/qwik"],
+        external: ['@builder.io/qwik'],
       },
     },
     plugins: [
       qwikNxVite(),
       qwikVite(),
       dts({
-        entryRoot: "src",
-        tsConfigFilePath: devkit.joinPathFragments(
-          __dirname,
-          "tsconfig.lib.json"
-        ),
+        entryRoot: 'src',
+        tsConfigFilePath: devkit.joinPathFragments(__dirname, 'tsconfig.lib.json'),
         skipDiagnostics: true,
       }),
       viteTsConfigPaths({
-        root: "../../",
+        root: '../../',
+      }),
+      viteStaticCopy({
+        targets: [
+          {
+            src: ['../../README.md', '../../LICENSE'],
+            dest: '.',
+          },
+        ],
       }),
     ],
     test: {
       globals: true,
       cache: {
-        dir: "../../node_modules/.vitest",
+        dir: '../../node_modules/.vitest',
       },
-      environment: "happy-dom",
+      environment: 'happy-dom',
       watch: false,
-      include: ["**/*.test.{ts,tsx}"],
+      include: ['**/*.test.{ts,tsx}'],
       clearMocks: true,
       coverage: {
         all: true,
         enabled: true,
-        provider: "istanbul",
-        include: ["src/**/**"],
-        exclude: ["src/root.tsx", "src/entry.dev.tsx", "src/entry.ssr.tsx"],
-        extension: [".js", ".ts", ".tsx"],
+        provider: 'istanbul',
+        include: ['src/**/**'],
+        exclude: ['src/root.tsx', 'src/entry.dev.tsx', 'src/entry.ssr.tsx'],
+        extension: ['.js', '.ts', '.tsx'],
         coverage: {
-          reportsDirectory: "../../coverage/libs/qwik-router",
+          reportsDirectory: '../../coverage/libs/qwik-router',
         },
       },
     },

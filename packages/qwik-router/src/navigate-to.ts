0f4f16a72bc4ada4b437f26efcfaf347cee6dbc0
Danny França <me@dannyfranca.com>
2023-09-24 11:05:39 +0100

    refactor: remove isBrowser check from navigateTo

--- packages/qwik-router/src/navigate-to.ts
+++ packages/qwik-router/src/navigate-to.ts
@@ -1,17 +1,13 @@
-import { isBrowser } from '@builder.io/qwik/build';
-
 import type { RouteState } from './types';
 import { generateNewUrlStateFromPath } from './utils/generate-new-url-from-path';
-import { getWindow } from './utils/get-window';
+import { getHistory } from './utils/get-window';
 import { updateRouteState } from './utils/update-route-state';
 
 /**
  * Navigate to a new path by pushing a new state to the history object. It updates the route state.
  */
 export function navigateTo(routeState: RouteState, path: string): void {
-  if (isBrowser) {
-    const newState = generateNewUrlStateFromPath(routeState, path);
-    getWindow()?.history?.pushState(newState, path, path);
-    updateRouteState(routeState, newState);
-  }
+  const newState = generateNewUrlStateFromPath(routeState.origin, path);
+  getHistory()?.pushState(newState, '', path);
+  updateRouteState(routeState, newState);
 }

9ed2e779025e088fa376d79905aef0f1c37f2285
Danny França <me@dannyfranca.com>
2023-09-24 11:48:11 +0100

    test: add routing test to ensure full state change

--- packages/qwik-router/src/routing.test.tsx
+++ packages/qwik-router/src/routing.test.tsx
@@ -7,7 +7,7 @@ import { initRouter, useNavigate, useRoute } from './routing';
 const StubNav = component$(() => {
   const nav = useNavigate();
   return (
-    <div class="navigate" onClick$={() => nav('/new-path')}>
+    <div class="navigate" onClick$={() => nav('/new-path?nqs=1#new-hash')}>
       navigate
     </div>
   );
@@ -18,6 +18,14 @@ const StubChild = component$(() => {
   return (
     <>
       <div class="stub-path">{routeState.pathname}</div>
+      <div class="stub-hash">{routeState.hash}</div>
+      <div class="stub-href">{routeState.href}</div>
+      <div class="stub-origin">{routeState.origin}</div>
+      <div class="stub-search">{routeState.search}</div>
+      <div class="stub-query">{JSON.stringify(routeState.query)}</div>
+      <div class="stub-search-params">{routeState.searchParams.toString()}</div>
+      <div class="stub-to-string">{routeState.toString()}</div>
+      <div class="stub-to-json">{routeState.toJSON()}</div>
     </>
   );
 });
@@ -34,25 +42,42 @@ const StubRoot = component$(({ url }: { url: string }) => {
 });
 
 describe('Qwik context', () => {
-  it('Render initial path', async () => {
-    const url = new URL('https://test.com/path?test=1#hash');
-    const { screen, render } = await createDOM();
+  it('Changes rendering after navigation', async () => {
+    const url = new URL('https://test.com/path?qs=1#hash');
+    const { screen, render, userEvent } = await createDOM();
 
     await render(<StubRoot url={url.toString()} />);
 
     const pathEl = screen.querySelector('.stub-path');
+    const hashEl = screen.querySelector('.stub-hash');
+    const hrefEl = screen.querySelector('.stub-href');
+    const originEl = screen.querySelector('.stub-origin');
+    const searchEl = screen.querySelector('.stub-search');
+    const queryEl = screen.querySelector('.stub-query');
+    const searchParamsEl = screen.querySelector('.stub-search-params');
+    const toStringEl = screen.querySelector('.stub-to-string');
+    const toJSONEl = screen.querySelector('.stub-to-json');
 
     expect(pathEl!.textContent).toBe('/path');
-  });
+    expect(hashEl!.textContent).toBe('#hash');
+    expect(hrefEl!.textContent).toBe('https://test.com/path?qs=1#hash');
+    expect(originEl!.textContent).toBe('https://test.com');
+    expect(searchEl!.textContent).toBe('?qs=1');
+    expect(queryEl!.textContent).toBe('{"qs":"1"}');
+    expect(searchParamsEl!.textContent).toBe('qs=1');
+    expect(toStringEl!.textContent).toBe('https://test.com/path?qs=1#hash');
+    expect(toJSONEl!.textContent).toBe('https://test.com/path?qs=1#hash');
 
-  it('Changes rendering after navigation', async () => {
-    const url = new URL('https://test.com/new-path?test=1#hash');
-    const { screen, render } = await createDOM();
-
-    await render(<StubRoot url={url.toString()} />);
-
-    const pathEl = screen.querySelector('.stub-path');
+    await userEvent('.navigate', 'click');
 
     expect(pathEl!.textContent).toBe('/new-path');
+    expect(hashEl!.textContent).toBe('#new-hash');
+    expect(hrefEl!.textContent).toBe('https://test.com/new-path?nqs=1#new-hash');
+    expect(originEl!.textContent).toBe('https://test.com');
+    expect(searchEl!.textContent).toBe('?nqs=1');
+    expect(queryEl!.textContent).toBe('{"nqs":"1"}');
+    expect(searchParamsEl!.textContent).toBe('nqs=1');
+    expect(toStringEl!.textContent).toBe('https://test.com/new-path?nqs=1#new-hash');
+    expect(toJSONEl!.textContent).toBe('https://test.com/new-path?nqs=1#new-hash');
   });
 });
